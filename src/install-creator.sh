d2c69dc862ff3080538ea45edeca5e1163ef6423
Matthias Klumpp <matthias@nlinux.org>
2010-02-18 20:54:54 +0100

    Updates for liCreator installer
    Made it possible to install the Qt4 and
    GTK2 version of liCreator parallel.
    Updated debianization.

--- src/install-creator.sh
+++ src/install-creator.sh
@@ -39,18 +39,24 @@ mkdir -p $DESTDIR/opt/appfiles/liCreator
 mkdir -p $DESTDIR$prefix/share/applications
 
 if [ "$WIDGET" == "qt4" ]; then
-cp '../build/qt4/licreator' $DESTDIR/opt/appfiles/liCreator/
-else
-cp '../build/gtk2/licreator' $DESTDIR/opt/appfiles/liCreator/
-fi
-
-cp '../graphics/listaller_creator.png' $DESTDIR/opt/appfiles/liCreator/
-cp "../licreator/licreator.desktop" $DESTDIR$prefix/share/applications/
-
+cp '../build/qt4/licreator-qt' $DESTDIR/opt/appfiles/liCreator/
+cp "../data/applications/licreator-qt.desktop" $DESTDIR$prefix/share/applications/
+cp '../graphics/listaller_creator.png' $DESTDIR/opt/appfiles/liCreator/listaller_creator-qt.png
 #Create symlink
-if [ ! -e "$DESTDIR$prefix/bin/licreator" ]; then
+if [ ! -e "$DESTDIR$prefix/bin/licreator-qt" ]; then
  cd $DESTDIR$prefix/bin
- ln -s /opt/appfiles/liCreator/licreator licreator
+ ln -s /opt/appfiles/liCreator/licreator-qt licreator-qt
 fi
+else
+cp '../build/gtk2/licreator-gtk' $DESTDIR/opt/appfiles/liCreator/
+cp "../data/applications/licreator-gtk.desktop" $DESTDIR$prefix/share/applications/
+cp '../graphics/listaller_creator.png' $DESTDIR/opt/appfiles/liCreator/listaller_creator-gtk.png
+#Create symlink
+if [ ! -e "$DESTDIR$prefix/bin/licreator-gtk" ]; then
+ cd $DESTDIR$prefix/bin
+ ln -s /opt/appfiles/liCreator/licreator-gtk licreator-gtk
+fi
+fi
+
 
 echo "Installation done. (liCreator)"
