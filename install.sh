742406cf8197622aab05032a60e0bf8498a42aee
Christian Beuschel <chris@merlin>
2010-03-04 14:57:46 +0100

    Enabled Setup for several distributions

--- install.sh
+++ install.sh
@@ -23,74 +23,27 @@ APPLIST="defaults.list"
 APPLINIK_I="pappi_install.desktop"
 APPLINIK_T="pappi_trust.desktop"
 
+# Reading information about the distribution
+LSB_RELEASE_FILE="/etc/lsb-release"
+
+if [ -f $LSB_RELEASE_FILE ]; then
+	source $LSB_RELEASE_FILE
+else
+	echo "Error: Installation LSB release file \"$LSB_RELEASE_FILE\" has not been found!"
+	exit 1
+fi
+
 # Installation
 
-echo " "
+INSTALLER_DIR="./distributions"
+SCRIPT=$INSTALLER_DIR'/'$DISTRIB_ID'_'$DISTRIB_RELEASE
 
-echo -n "Copying files ..."
 
-mkdir $INSTDIR
-cp -r ./files/* $INSTDIR/
-chmod +x $INSTDIR/bin/pappi
-chmod +x $INSTDIR/bin/uninstall
-ln -s $INSTDIR/bin/pappi /usr/local/bin/pappi
-
-echo '... done'
-echo " "
-
-echo -n "Registering mime-types ..."
-
-if [ ! -d "$MIMEDB" ]; then
-	mkdir -p $MIMEDB
+if [ -f $SCRIPT ]; then
+	source $SCRIPT
+else
+	echo "Error: Your Distribution or your Version of $DISTRIB_ID is not supported yet. If you are a programmer, help us to support it."
+	exit 1
 fi
-if [ ! -d "$APPDB" ]; then
-	mkdir -p $APPDB
-fi
-
-echo '<?xml version="1.0" encoding="UTF-8"?>' >> $MIMEDB'/'$MIMEFILE
-
-echo '<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">' >> $MIMEDB'/'$MIMEFILE
-echo '  <mime-type type="application/x-personal-application-package">' >> $MIMEDB'/'$MIMEFILE
-echo '    <comment>Personal Application Package</comment>' >> $MIMEDB'/'$MIMEFILE
-echo '    <glob pattern="*.pappp"/>' >> $MIMEDB'/'$MIMEFILE
-echo '    <generic-icon name="package-x-generic"/>' >> $MIMEDB'/'$MIMEFILE
-echo '  </mime-type>' >> $MIMEDB'/'$MIMEFILE
-
-echo '  <mime-type type="application/x-personal-application-store-trust-file">' >> $MIMEDB'/'$MIMEFILE
-echo '    <comment>Personal Application Store Trust File</comment>' >> $MIMEDB'/'$MIMEFILE
-echo '    <glob pattern="*.pappst"/>' >> $MIMEDB'/'$MIMEFILE
-echo '    <generic-icon name="package-x-generic"/>' >> $MIMEDB'/'$MIMEFILE
-echo '  </mime-type>' >> $MIMEDB'/'$MIMEFILE
-
-echo '</mime-info>' >> $MIMEDB'/'$MIMEFILE
-
-if [ ! -f $APPDB/$APPLIST ]; then
-	echo "[Default Applications]" > $APPDB/$APPLIST
-fi
-echo "application/x-personal-application-package=pappi_install.desktop" >> $APPDB/$APPLIST
-echo "application/x-personal-application-store-trust-file=pappi_trust.desktop" >> $APPDB/$APPLIST
-
-echo '[Desktop Entry]' >> $APPDB/$APPLINIK_I
-echo 'Version=1.0' >> $APPDB/$APPLINIK_I
-echo 'Encoding=UTF-8' >> $APPDB/$APPLINIK_I
-echo 'Name=Personal App Installer' >> $APPDB/$APPLINIK_I
-echo 'Terminal=false' >> $APPDB/$APPLINIK_I
-echo 'Exec=/opt/pappi/bin/pappi -i %U' >> $APPDB/$APPLINIK_I
-echo 'Icon=/opt/pappi/icons/pappi_48.png' >> $APPDB/$APPLINIK_I
-echo 'Type=Application' >> $APPDB/$APPLINIK_I
-
-
-update-mime-database $MIMEBASE
-
-echo '... done'
-echo " "
-
-echo "Installing dependenzies ..."
-
-apt-get -y install uae wine dosbox zenity
-
-echo '... done'
-
-echo 'Installation of pappi complete.'
 
 exit 0
