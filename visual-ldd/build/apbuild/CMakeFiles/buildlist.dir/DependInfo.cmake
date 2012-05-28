42c8a8d3f10b02469f568849494497d738e2a02a
Matthias Klumpp <matthias@tenstral.net>
2012-05-29 01:20:26 +0200

    Prepared for visual-LDD merge

---
+++ visual-ldd/build/apbuild/CMakeFiles/buildlist.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/matthias/Development/Listaller/listaller-devtools/build/apbuild/buildlist.c" "/home/matthias/Development/Listaller/listaller-devtools/build/apbuild/CMakeFiles/buildlist.dir/buildlist.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "CMAKE_INSTALL_PREFIX=\"\\\"/usr\\\"\""
+  "CMAKE_INSTALL_LIBDIR=\"\\\"\\\"\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
